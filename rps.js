0e33e708ea6cd883fb9e8d80a521af8d48692258
Rehman Javaid <rehmanjav@outlook.com>
2022-07-12 16:18:47 -0400

    Create computerPlay() function

--- rps.js
+++ rps.js
@@ -1,1 +1,10 @@
 "use strict";
+
+function computerPlay() {
+    let choice = Math.random()
+
+    if (choice <= 0.333 && choice >= 0) {return "rock"}
+    else if (choice <= 0.666 && choice > 0.333) {return "paper"}
+    else if (choice <= 1 && choice > 0.666) {return "scissors"}
+};
+
