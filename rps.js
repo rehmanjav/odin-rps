c5d6212d6532b93831c61a041ca3c76358763c22
Rehman Javaid <rehmanjav@outlook.com>
2022-07-12 16:52:26 -0400

    Create playRound() function

--- rps.js
+++ rps.js
@@ -6,5 +6,23 @@ function computerPlay() {
     if (choice <= 0.333 && choice >= 0) {return "rock"}
     else if (choice <= 0.666 && choice > 0.333) {return "paper"}
     else if (choice <= 1 && choice > 0.666) {return "scissors"}
-};
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if ((playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper")) {
+        return `You win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}.`
+    } else if ((playerSelection == "rock" && computerSelection == "paper") ||
+               (playerSelection == "paper" && computerSelection == "scissors") ||
+               (playerSelection == "scissors" && computerSelection == "rock")) {
+        return `You lose! ${playerSelection.toUpperCase()} loses to ${computerSelection.toUpperCase()}.`
+    } else if ((playerSelection == "rock" && computerSelection == "rock") ||
+               (playerSelection == "paper" && computerSelection == "paper") ||
+               (playerSelection == "scissors" && computerSelection == "scissors")) {
+        return `It is a tie! ${playerSelection.toUpperCase()} ties with ${computerSelection.toUpperCase()}.`
+    }
+}
 
