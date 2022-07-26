2a0707c1a4e07f6fd7d92a9ed96b080a10cd26d1
Rehman Javaid <rehmanjav@outlook.com>
2022-07-26 16:55:29 -0400

    Add logic to set dialog

--- rps.js
+++ rps.js
@@ -5,34 +5,67 @@ function computerPlay() {
 
     if (choice <= 0.333 && choice >= 0) {return "rock"}
     else if (choice <= 0.666 && choice > 0.333) {return "paper"}
-    else if (choice <= 1 && choice > 0.666) {return "scissors"}
+    else if (choice <= 1 && choice > 0.666) {return "scissor"}
 }
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
 
-    if ((playerSelection == "rock" && computerSelection == "scissors") ||
+    if ((playerSelection == "rock" && computerSelection == "scissor") ||
         (playerSelection == "paper" && computerSelection == "rock") ||
-        (playerSelection == "scissors" && computerSelection == "paper")) {
+        (playerSelection == "scissor" && computerSelection == "paper")) {
         return `You win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}.`
     } else if ((playerSelection == "rock" && computerSelection == "paper") ||
-               (playerSelection == "paper" && computerSelection == "scissors") ||
-               (playerSelection == "scissors" && computerSelection == "rock")) {
+               (playerSelection == "paper" && computerSelection == "scissor") ||
+               (playerSelection == "scissor" && computerSelection == "rock")) {
         return `You lose! ${playerSelection.toUpperCase()} loses to ${computerSelection.toUpperCase()}.`
     } else if ((playerSelection == "rock" && computerSelection == "rock") ||
                (playerSelection == "paper" && computerSelection == "paper") ||
-               (playerSelection == "scissors" && computerSelection == "scissors")) {
+               (playerSelection == "scissor" && computerSelection == "scissor")) {
         return `It is a tie! ${playerSelection.toUpperCase()} ties with ${computerSelection.toUpperCase()}.`
     }
 }
 
-function game() {
-    console.log("Lets play a game of Rock Paper Scissors!")
+function game(playerSelection) {
+    let result = playRound(playerSelection, computerPlay());
     
-    let playerSelection = prompt("Please enter your play.", "");
-    console.log(playRound(playerSelection, computerPlay()));
+
+    
+
+    // if (result[4] == 'w') {
+    //     wins += 1;
+    // } else if (result[4] == 'l') {
+    //     losses += 1;
+    // } else if (result[4] == 's') {
+    //     ties += 1;
+    // }
+
+    // Update dialog
+    let dialog = document.querySelector(".dialog");
+    dialog.textContent = "Please make another selection to play another round.";
+    
+
+    // Update round
+    console.log('round ' + round);
+    let round = document.querySelector(".round");
+
+    // Update score
+    console.log(`wins:${wins} losses:${losses} ties:${ties}`);
+    let score = document.querySelector(".score");
+
+    // Update result
+    let resultSpan = document.querySelector(".result");
+    resultSpan.textContent = result;
     
 }
 
-game();
+// PROGRAM BEGINS
+
+let buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => button.addEventListener('click', (e) => {
+    game(e.target.classList[0]);
+}));
+
+
 
